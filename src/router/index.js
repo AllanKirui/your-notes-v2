5efc58ef5dc5acaf489dcd4d6c76002e4e170edd
AllanKirui <nallawilljr@gmail.com>
2022-07-14 14:44:30 +0300

    Add and register routing for the LogIn cmp

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import SignUp from "@/components/authn/SignUp.vue";
+import LogIn from "@/components/authn/LogIn.vue";
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/signup" },
     { name: "signup", path: "/signup", components: { authn: SignUp } },
+    { name: "login", path: "/login", components: { authn: LogIn } },
   ],
 });
