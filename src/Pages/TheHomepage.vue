f439b23298e0fa2305da7b30ef33dc5a9846e3c2
AllanKirui <nallawilljr@gmail.com>
2022-07-16 11:44:53 +0300

    Add and register TheHomepage cmp

---
+++ src/Pages/TheHomepage.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Welcome Home</h1>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import SignUp from "@/pages/SignUp.vue";
 import LogIn from "@/pages/LogIn.vue";
+import TheHomepage from "@/pages/TheHomepage.vue";
 
 export default createRouter({
   history: createWebHistory(),
@@ -9,5 +10,6 @@ export default createRouter({
     { path: "/", redirect: "/signup" },
     { name: "signup", path: "/signup", components: { authn: SignUp } },
     { name: "login", path: "/login", components: { authn: LogIn } },
+    { name: "home", path: "/home", component: TheHomepage },
   ],
 });
