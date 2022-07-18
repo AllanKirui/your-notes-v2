bf924e0cc10e67e586e03c383221a2e1b4dd5b93
AllanKirui <nallawilljr@gmail.com>
2022-07-18 11:25:04 +0300

    Add and register TheSidebar cmp

--- src/Pages/TheHomepage.vue
+++ src/Pages/TheHomepage.vue
@@ -1,13 +1,19 @@
 <template>
   <the-header></the-header>
+
+  <div class="container">
+    <the-sidebar></the-sidebar>
+  </div>
 </template>
 
 <script>
 import TheHeader from "@/components/nav/TheHeader.vue";
+import TheSidebar from "@/components/ui/TheSidebar.vue";
 
 export default {
   components: {
     TheHeader,
+    TheSidebar,
   },
 };
 </script>
---
+++ src/components/ui/TheSidebar.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Sidebar</h1>
+</template>
