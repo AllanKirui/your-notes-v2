be285b335fb8f24d5cede49d2f53c00baf636122
AllanKirui <nallawilljr@gmail.com>
2022-07-19 16:14:35 +0300

    Add logic to get and display data from vuex store

--- src/components/todos/TodoList.vue
+++ src/components/todos/TodoList.vue
@@ -1,15 +1,27 @@
 <template>
+  <!-- TODO: only show this if there are todos -->
   <ul>
-    <todo-item></todo-item>
+    <todo-item
+      v-for="todo of todoList"
+      :key="todo.id"
+      :title="todo.title"
+      :contents="todo.contents"
+    ></todo-item>
   </ul>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 import TodoItem from "@/components/todos/TodoItem.vue";
 
 export default {
   components: {
     TodoItem,
+  },
+  computed: {
+    // tap into the 'todos' module and get the 'todoList' from the store
+    ...mapGetters("todos", ["todoList"]),
   },
 };
 </script>
