5c27b87ab8e1a598bcd47b10ba93be7bd7d207c6
AllanKirui <nallawilljr@gmail.com>
2022-07-19 12:18:10 +0300

    Add update to route config

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import SignUp from "@/pages/SignUp.vue";
 import LogIn from "@/pages/LogIn.vue";
 import TheTodos from "@/pages/TheTodos.vue";
+import TheNotes from "@/pages/TheNotes.vue";
+import TheSettings from "@/pages/TheSettings.vue";
 
 export default createRouter({
   history: createWebHistory(),
@@ -11,5 +13,7 @@ export default createRouter({
     { name: "signup", path: "/signup", components: { authn: SignUp } },
     { name: "login", path: "/login", components: { authn: LogIn } },
     { name: "todos", path: "/todos", component: TheTodos },
+    { name: "notes", path: "/notes", component: TheNotes },
+    { name: "settings", path: "/settings", component: TheSettings },
   ],
 });
