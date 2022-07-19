3e1d5969c38a0bf81108427c68103f4b40d3abd9
AllanKirui <nallawilljr@gmail.com>
2022-07-19 12:53:08 +0300

    Add import for todos vuex module

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 
 import authModule from "@/store/modules/auth/index";
+import todoModule from "@/store/modules/todos/index";
 
 export default createStore({
   modules: {
     auth: authModule,
+    todos: todoModule,
   },
   state() {},
   mutations: {},
