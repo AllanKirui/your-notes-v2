da38f9119bc54a0376b88bc5f36ab96632d4df4f
AllanKirui <nallawilljr@gmail.com>
2022-07-19 21:25:52 +0300

    Add logic to get details of a selected todo item

--- src/components/todos/TodoDetails.vue
+++ src/components/todos/TodoDetails.vue
@@ -1,3 +1,32 @@
 <template>
-  <h4>Here are your details</h4>
+  <div v-if="hasTodo" class="details-wrapper">
+    <h3>{{ selectedTodo.title }}</h3>
+    <ul>
+      <li v-for="(item, index) of selectedTodo.contents" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  data() {
+    return {
+      hasTodo: false,
+    };
+  },
+  computed: {
+    ...mapGetters("todos", ["selectedTodo"]),
+  },
+  watch: {
+    selectedTodo(newValue) {
+      if (newValue) {
+        this.hasTodo = true;
+      }
+    },
+  },
+};
+</script>
