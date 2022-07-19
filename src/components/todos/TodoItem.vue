373fdfb2be92d9e7c0751e66fb0cde392e389e80
AllanKirui <nallawilljr@gmail.com>
2022-07-19 16:12:57 +0300

    Add html, styles for TodoItem cmp

--- src/components/todos/TodoItem.vue
+++ src/components/todos/TodoItem.vue
@@ -1,3 +1,56 @@
 <template>
-  <li>This is a todo item</li>
+  <li class="item-wrapper" :title="hoverTitle">
+    <h4 class="item-title">{{ title }}</h4>
+    <ul class="item-list">
+      <!-- Get the first 3 items of the contents -->
+      <li class="item flex flex-ai-c" v-for="(_, index) in 3" :key="index">
+        <img src="@/assets/img/checkbox-grey.svg" alt=" " width="14" />
+        <span> {{ contents[index] }} </span>
+      </li>
+    </ul>
+  </li>
 </template>
+
+<script>
+export default {
+  props: ["title", "contents"],
+  computed: {
+    hoverTitle() {
+      return "View " + this.title;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.item-wrapper {
+  padding: 0.5rem;
+  outline: 1px solid var(--color-platinum);
+  cursor: pointer;
+}
+
+.item-wrapper.active {
+  border-left: 2px solid tomato;
+}
+
+.item-wrapper.inactive {
+  border-left: none;
+}
+
+.item-list,
+.item-title {
+  font-size: 0.875rem;
+}
+
+.item-list {
+  margin-top: 0.5rem;
+}
+
+.item:not(:first-child) {
+  margin-top: 2px;
+}
+
+.item span {
+  margin-left: 0.5rem;
+}
+</style>
