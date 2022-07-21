20a0fcf5eb80ab1ec415a7ae5b5176774f94595c
AllanKirui <nallawilljr@gmail.com>
2022-07-21 14:36:25 +0300

    Add html, logic to show todo details

--- src/components/todos/TodoDetails.vue
+++ src/components/todos/TodoDetails.vue
@@ -1,9 +1,37 @@
 <template>
   <div v-if="hasTodo" class="details-wrapper">
-    <h3>{{ selectedTodo.title }}</h3>
-    <ul>
+    <h3 class="items-title">{{ selectedTodo.title }}</h3>
+    <ul class="items">
       <li v-for="(item, index) of selectedTodo.contents" :key="index">
-        {{ item }}
+        <div class="item flex">
+          <div class="item-checkbox">
+            <input type="checkbox" />
+          </div>
+
+          <div class="item-text-wrapper">
+            <div class="item-text" v-if="selectedItem !== index">
+              <span class="item-details" @click="editText(index)">{{
+                item
+              }}</span>
+            </div>
+            <!-- only show for the clicked todo item -->
+            <div
+              class="item-edit-field"
+              v-if="isEditText && selectedItem === index"
+            >
+              <textarea
+                class="field"
+                type="text"
+                :value="item"
+                ref="textarea"
+              ></textarea>
+              <div class="edit-controls">
+                <button>Save</button>
+                <button>Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -16,14 +44,36 @@ export default {
   data() {
     return {
       hasTodo: false,
+      isEditText: false,
+      selectedItem: null,
     };
   },
   computed: {
     ...mapGetters("todos", ["selectedTodo"]),
   },
+  methods: {
+    editText(index) {
+      this.selectedItem = index;
+      this.isEditText = true;
+
+      // resize the textarea after it has been rendered
+      this.$nextTick(() => {
+        this.autoResizeField();
+      });
+    },
+    autoResizeField() {
+      let textarea = this.$refs.textarea[0];
+      textarea.style.height = textarea.scrollHeight + "px";
+
+      // focus on the textare and highlight its contents
+      textarea.focus();
+      textarea.select();
+    },
+  },
   watch: {
     selectedTodo(newValue) {
       if (newValue) {
+        this.selectedItem = null; // reset the selectedItem prop
         this.hasTodo = true;
       }
     },
