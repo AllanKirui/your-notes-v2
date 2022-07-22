020efd4e825cd28e6435d2f0236c6938ffc7a8ee
AllanKirui <nallawilljr@gmail.com>
2022-07-22 14:49:16 +0300

    Add update to todos structure

--- src/store/modules/todos/index.js
+++ src/store/modules/todos/index.js
@@ -8,23 +8,38 @@ export default {
     return {
       todos: [
         {
-          id: 1,
+          id: 0,
           title: "Shopping list",
-          contents: ["Milk", "Sugar", "Salt", "Butter", "Bread"],
+          contents: [
+            { text: "Milk", isCompleted: false },
+            { text: "Sugar", isCompleted: false },
+            { text: "Salt", isCompleted: false },
+            { text: "Butter", isCompleted: false },
+            { text: "Bread", isCompleted: false },
+          ],
         },
         {
-          id: 2,
+          id: 1,
           title: "Book reading list",
           contents: [
-            "Atomic Habits by James Clear",
-            "Deep Work by Cal Newport",
-            "The Power of Habit by Charles Duhigg",
-            "The Talent Code by Daniel Coyle",
-            "CSS Secrets by Lea Verou",
-            "Cracking the Coding Interview by Gayle Laakman",
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem excepturi illum sint optio nostrum distinctio voluptas, perspiciatis in esse eos possimus! Enim eius quasi laudantium, harum dolor esse quo, atque quos quisquam officia ipsam eum exercitationem magnam minima? Dicta architecto cumque hic quod et eius nulla itaque ducimus quos sint nobis veniam aut rerum, sequi explicabo, quisquam ullam. Id, sint?",
-            "The Markdown Guide by Mark Cone",
-            "Heaven is For Real by Todd Burpo",
+            { text: "Atomic Habits by James Clear", isCompleted: false },
+            { text: "Deep Work by Cal Newport", isCompleted: false },
+            {
+              text: "The Power of Habit by Charles Duhigg",
+              isCompleted: false,
+            },
+            { text: "The Talent Code by Daniel Coyle", isCompleted: false },
+            { text: "CSS Secrets by Lea Verou", isCompleted: false },
+            {
+              text: "Cracking the Coding Interview by Gayle Laakman",
+              isCompleted: false,
+            },
+            {
+              text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem excepturi illum sint optio nostrum distinctio voluptas, perspiciatis in esse eos possimus! Enim eius quasi laudantium, harum dolor esse quo, atque quos quisquam officia ipsam eum exercitationem magnam minima? Dicta architecto cumque hic quod et eius nulla itaque ducimus quos sint nobis veniam aut rerum, sequi explicabo, quisquam ullam. Id, sint?",
+              isCompleted: false,
+            },
+            { text: "The Markdown Guide by Mark Cone", isCompleted: false },
+            { text: "Heaven is For Real by Todd Burpo", isCompleted: false },
           ],
         },
       ],
