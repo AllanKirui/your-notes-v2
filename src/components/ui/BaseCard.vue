f329a12f5c3d370251fa7375bf7828fc7caf92b5
AllanKirui <nallawilljr@gmail.com>
2022-07-30 19:10:09 +0300

    Add update to scoped styles

--- src/components/ui/BaseCard.vue
+++ src/components/ui/BaseCard.vue
@@ -10,7 +10,7 @@
   margin: 0 2rem;
   width: 100%;
   max-width: 35rem;
-  border-radius: 5px;
+  border-radius: 8px;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   background-color: var(--color-white);
 }
