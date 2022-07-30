51a59d2ded589d17fb40af16ce61e78c98f7886b
AllanKirui <nallawilljr@gmail.com>
2022-07-30 10:27:57 +0300

    Add logic to show welcome message

--- src/store/index.js
+++ src/store/index.js
@@ -8,8 +8,45 @@ export default createStore({
     auth: authModule,
     todos: todoModule,
   },
-  state() {},
-  mutations: {},
-  actions: {},
-  getters: {},
+  state() {
+    return {
+      greeting: "",
+    };
+  },
+  mutations: {
+    setGreeting(state, payload) {
+      state.greeting = payload;
+    },
+  },
+  actions: {
+    setGreeting(context) {
+      let today = new Date();
+      let hour = today.getHours();
+      let greeting = "";
+      let trimmedName = "";
+
+      // if name is multi-worded, use the first word of the name
+      let wordsInName = context.state.auth.user.displayName.split(" ");
+      if (wordsInName.length > 0) {
+        trimmedName = wordsInName[0];
+      }
+
+      // set an appropriate greeting based on the time
+      if (hour >= 16) {
+        greeting = `Good evening, ${trimmedName}`;
+      } else if (hour >= 12) {
+        greeting = `Good afternoon, ${trimmedName}`;
+      } else if (hour >= 0) {
+        greeting = `Good morning, ${trimmedName}`;
+      }
+
+      // commit a mutation to set the greeting
+      context.commit("setGreeting", greeting);
+    },
+  },
+  getters: {
+    greeting(state) {
+      return state.greeting;
+    },
+  },
 });
