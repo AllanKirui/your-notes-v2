01125ef2eae99cdccd4204662560a1a85ba9fe69
AllanKirui <nallawilljr@gmail.com>
2022-08-08 12:23:20 +0300

    Add logic to set text length

--- src/components/notes/NotesItem.vue
+++ src/components/notes/NotesItem.vue
@@ -1,8 +1,15 @@
 <template>
-  <li class="item-wrapper">
+  <li class="item-wrapper flex">
+    <div class="indicator">
+      <span class="active"></span>
+    </div>
+
     <div class="text-wrapper">
       <div class="title-wrapper flex flex-jc-sb">
         <h4 class="item-title">{{ title }}</h4>
+      </div>
+      <div class="item-text">
+        <p class="text-contents">{{ setTextLength(content, 200) }}</p>
       </div>
     </div>
   </li>
@@ -10,6 +17,7 @@
 
 <script>
 export default {
-  props: ["id", "title", "contents"],
+  props: ["id", "title", "content"],
+  inject: ["setTextLength"],
 };
 </script>
