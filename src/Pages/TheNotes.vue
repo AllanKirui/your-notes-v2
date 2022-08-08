97635e515ca6ac5a8ccc6ee70ef1dfbbdc8753da
AllanKirui <nallawilljr@gmail.com>
2022-08-08 11:29:15 +0300

    Add and register components

--- src/Pages/TheNotes.vue
+++ src/Pages/TheNotes.vue
@@ -11,16 +11,46 @@
       </div>
     </div>
   </section>
+
+  <section class="content-items" v-if="numOfNotes">
+    <h3 class="items-title">All Notes</h3>
+    <notes-list></notes-list>
+  </section>
+
+  <section class="content-items-details" v-if="numOfNotes">
+    <div class="bg-images"></div>
+    <notes-details @show-notification="showNotification"></notes-details>
+  </section>
 </template>
 
 <script>
+import NotesList from "@/components/notes/NotesList.vue";
+import NotesDetails from "@/components/notes/NotesDetails.vue";
+
 export default {
+  props: ["isModal"],
+  emits: ["close-modal", "show-notification"],
+  components: {
+    NotesList,
+    NotesDetails,
+  },
   computed: {
     numOfNotes() {
       return this.$store.getters["notes/numOfNotes"];
     },
     greeting() {
       return this.$store.getters.greeting;
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close-modal");
+
+      // dispatch an action to reset the isCloseOpenField state prop
+      this.$store.dispatch("todos/closeOpenFields", false);
+    },
+    showNotification(message) {
+      this.$emit("show-notification", message);
     },
   },
 };
---
+++ src/components/notes/NotesDetails.vue
@@ -0,0 +1,3 @@
+<template>
+  <h3>Notes Details</h3>
+</template>
