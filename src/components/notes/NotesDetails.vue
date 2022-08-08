b783e78c965142b42e273f9a726c525176eb06d8
AllanKirui <nallawilljr@gmail.com>
2022-08-09 02:08:41 +0300

    Add html, logic, styles to show selected note

--- src/components/notes/NotesDetails.vue
+++ src/components/notes/NotesDetails.vue
@@ -1,3 +1,50 @@
 <template>
-  <h3>Notes Details</h3>
+  <div v-if="!hasNote" class="intro-wrapper">
+    <h3 class="intro-title">{{ greeting }}</h3>
+    <p class="intro-message-1">
+      Please select a note from the <b>All Notes</b> panel to continue working
+      on an existing note
+    </p>
+    <p class="intro-message-2">
+      Or click the <b>Create New Note</b> button on the sidebar to create a new
+      note
+    </p>
+  </div>
+
+  <!-- Notes title -->
+  <div v-if="hasNote" class="heading-wrapper">
+    <div class="heading-top flex">
+      <h3 class="items-title">{{ selectedNote.title }}</h3>
+    </div>
+  </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  emits: ["show-notification"],
+  data() {
+    return {
+      hasNote: false,
+    };
+  },
+  computed: {
+    ...mapGetters("notes", ["selectedNote"]),
+    ...mapGetters(["greeting"]),
+  },
+  watch: {
+    selectedNote(newNote) {
+      if (newNote) {
+        this.hasNote = true;
+      }
+    },
+  },
+  beforeUpdate() {
+    const note = this.$store.getters["notes/selectedNote"];
+
+    // dispatch an action to set the open note id
+    this.$store.dispatch("notes/setOpenNoteId", note.id);
+  },
+};
+</script>
--- src/components/notes/NotesItem.vue
+++ src/components/notes/NotesItem.vue
@@ -1,11 +1,15 @@
 <template>
-  <li class="item-wrapper flex" :title="hoverTitle">
+  <li
+    :class="[openNoteId === id ? 'active' : '', 'item-wrapper flex']"
+    :title="hoverTitle"
+    @click="setSelectedNote(id)"
+  >
     <div class="indicator">
       <span class="active"></span>
     </div>
 
     <div class="text-wrapper">
-      <div class="title-wrapper flex flex-jc-sb">
+      <div class="title-wrapper">
         <h4 class="item-title">{{ title }}</h4>
       </div>
       <div class="item-text">
@@ -22,6 +26,18 @@ export default {
   computed: {
     hoverTitle() {
       return "View " + this.title;
+    },
+    openNoteId() {
+      return this.$store.getters["notes/openNoteId"];
+    },
+  },
+  methods: {
+    setSelectedNote(id) {
+      // dispatch an action to set the id of the selected notes item
+      this.$store.dispatch({
+        type: "notes/setSelectedNote",
+        noteId: id,
+      });
     },
   },
 };
