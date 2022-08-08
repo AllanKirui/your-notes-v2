cac11a011ae80c01d81aaf8bf686adcd2b6c52d9
AllanKirui <nallawilljr@gmail.com>
2022-08-08 11:46:09 +0300

    Add and register NotesItem cmp

---
+++ src/components/notes/NotesItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <li class="item-wrapper">
+    <div class="text-wrapper">
+      <div class="title-wrapper flex flex-jc-sb">
+        <h4 class="item-title">{{ title }}</h4>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["id", "title", "contents"],
+};
+</script>
--- src/components/notes/NotesList.vue
+++ src/components/notes/NotesList.vue
@@ -1,3 +1,25 @@
 <template>
-  <h3>Notes List</h3>
+  <ul>
+    <notes-item
+      v-for="note of notesList"
+      :key="note.id"
+      :id="note.id"
+      :title="note.title"
+      :content="note.content"
+    ></notes-item>
+  </ul>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+
+import NotesItem from "@/components/notes/NotesItem.vue";
+
+export default {
+  components: { NotesItem },
+  computed: {
+    // tap into the 'notes' module and get the 'notesList' from the store
+    ...mapGetters("notes", ["notesList"]),
+  },
+};
+</script>
