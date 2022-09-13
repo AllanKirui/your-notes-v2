3c1f98901441a3f67312d1dfcf1b6fa2a5c9cac3
AllanKirui <nallawilljr@gmail.com>
2022-09-13 19:07:12 +0300

    Add logic, styles for purplish theme

--- src/Pages/TheSettings.vue
+++ src/Pages/TheSettings.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="settings-wrapper flex flex-jc-c">
-    <base-card mode="default-theme">
+    <base-card :mode="cardStyle">
       <div class="title">
         <h2 class="title-text">Settings</h2>
       </div>
@@ -26,11 +26,15 @@
         </div>
 
         <div class="options-contents">
+          <!-- appearance settings -->
           <div class="content" v-if="activeSide === 'appearance'">
             <h3 class="content-title">Choose Your Theme</h3>
             <p class="content-meta">Select how Your Notes looks to you.</p>
             <div class="themes-wrapper flex flex-jc-c flex-fw-w">
-              <div class="wrapper">
+              <div
+                :class="[!theme ? 'active' : '', 'wrapper']"
+                @click="setTheme('default')"
+              >
                 <div class="theme theme-1"></div>
                 <div class="theme-options flex flex-ai-c flex-jc-c">
                   <span class="dot"></span>
@@ -38,7 +42,10 @@
                 </div>
               </div>
 
-              <div class="wrapper">
+              <div
+                :class="[theme === 'purplish' ? 'active' : '', 'wrapper']"
+                @click="setTheme('purplish')"
+              >
                 <div class="theme theme-2"></div>
                 <div class="theme-options flex flex-ai-c flex-jc-c">
                   <span class="dot"></span>
@@ -47,6 +54,8 @@
               </div>
             </div>
           </div>
+
+          <!-- password settings -->
           <div class="content" v-else-if="activeSide === 'passwords'">
             <h3 class="content-title">Change Your Password</h3>
           </div>
@@ -63,9 +72,39 @@ export default {
       activeSide: "appearance",
     };
   },
+  computed: {
+    theme() {
+      return this.$store.getters.theme;
+    },
+    cardStyle() {
+      let mode = "";
+
+      // for default theme
+      if (!this.theme) {
+        mode = "default-theme";
+      }
+
+      // for purplish theme
+      if (this.theme === "purplish") {
+        mode = "purplish-theme";
+      }
+
+      return mode;
+    },
+  },
   methods: {
     setActiveSide(side) {
       this.activeSide = side;
+    },
+    setTheme(theme) {
+      if (theme === "default") {
+        // dispatch an action to set the default theme
+        this.$store.dispatch("setTheme", null);
+        return;
+      }
+
+      // dispatch an action to set any other selected theme
+      this.$store.dispatch("setTheme", theme);
     },
   },
 };
@@ -73,28 +112,28 @@ export default {
 
 <style scoped>
 .settings-wrapper {
-  padding-top: 60px;
-  /* width: 100%; */
+  padding-top: 3.75rem;
 }
 
 .settings-wrapper .card {
-  /* max-width: calc(100% - 64px); */
-  max-width: 50rem;
-  /* margin: 0; */
-  height: fit-content;
   padding: 0;
+  max-width: 50rem;
+  height: fit-content;
+}
+
+.purplish-theme .settings-wrapper {
+  background-color: var(--color-spanish-pink);
 }
 
 .title {
-  /* background-color: tomato; */
-  padding: 15px 20px 15px;
+  padding: 1rem 1.25rem;
 }
 
 .options {
   display: grid;
-  grid-template-columns: 200px 1fr;
+  grid-template-columns: 12.5rem 1fr;
   grid-row: 1fr;
-  min-height: 360px;
+  min-height: 22.5rem;
 }
 
 .options-links-wrapper {
@@ -110,7 +149,7 @@ export default {
 
 .options-links .link {
   font-weight: 500;
-  padding: 10px 16px;
+  padding: 0.625rem 1rem;
   cursor: pointer;
   transition: all 0.15s ease-in-out;
 }
@@ -125,6 +164,16 @@ export default {
   background-color: var(--color-graphite-black);
 }
 
+.purplish-theme .options-links .link.active:hover,
+.purplish-theme .options-links .link:hover {
+  background-color: var(--color-spanish-pink);
+  color: var(--color-black-blue);
+}
+
+.purplish-theme .options-links .link.active {
+  background-color: var(--color-russian-violet);
+}
+
 .options-contents {
   grid-column: 2;
   grid-row: 1;
@@ -134,30 +183,36 @@ export default {
   background-color: var(--color-white);
 }
 
+.purplish-theme .options-contents {
+  background-color: var(--color-russian-violet);
+}
+
 .content {
   text-align: center;
 }
 
 .content .content-title {
-  margin-top: 30px;
+  margin-top: 1.875rem;
 }
 
 .content .content-meta {
-  margin-top: 10px;
+  margin-top: 0.625rem;
 }
 
 .content .themes-wrapper {
-  gap: 20px;
-  flex-wrap: wrap;
-  margin-top: 30px;
+  gap: 1.25rem;
+  margin-top: 1.875rem;
   width: 100%;
 }
 
+.content .themes-wrapper .wrapper {
+  cursor: pointer;
+}
+
 .content .themes-wrapper .wrapper .theme {
-  width: 228px;
-  height: 120px;
+  width: 14.25rem;
+  height: 7.5rem;
   border-radius: 5px;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   background-repeat: no-repeat;
   background-size: contain;
   background-position: center center;
@@ -165,6 +220,11 @@ export default {
 
 .default-theme .content .themes-wrapper .wrapper .theme {
   background-color: var(--color-clouds);
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+
+.purplish-theme .content .themes-wrapper .wrapper .theme {
+  box-shadow: rgba(255, 122, 175, 0.3) 0px 3px 8px;
 }
 
 .content .themes-wrapper .wrapper .theme.theme-1 {
@@ -175,16 +235,33 @@ export default {
   background-image: url("@/assets/img/purplish-theme.svg");
 }
 
-.theme-options {
-  margin-top: 10px;
+.content .themes-wrapper .wrapper .theme-options {
+  margin-top: 1rem;
 }
 
-.theme-options input,
-.theme-options label {
-  cursor: pointer;
+.content .themes-wrapper .wrapper .theme-options .dot {
+  width: 0.75rem;
+  height: 0.75rem;
+  margin-right: 0.625rem;
+  border-radius: 50px;
+  outline-offset: 2px;
 }
 
-.theme-options label {
-  margin-left: 3px;
+.default-theme .wrapper .theme-options .dot {
+  outline: 2px solid var(--color-eerie-black);
+}
+
+.default-theme .wrapper.active .theme-options .dot {
+  outline: 2px solid var(--color-eerie-black);
+  background-color: var(--color-eerie-black);
+}
+
+.purplish-theme .wrapper .theme-options .dot {
+  outline: 2px solid var(--color-tickle-me-pink);
+}
+
+.purplish-theme .wrapper.active .theme-options .dot {
+  outline: 2px solid var(--color-tickle-me-pink);
+  background-color: var(--color-tickle-me-pink);
 }
 </style>
