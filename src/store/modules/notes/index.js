d212a21e6730b1e3f38c108934f7a75014bbbb9d
AllanKirui <nallawilljr@gmail.com>
2022-11-04 10:53:11 +0300

    Add update to content for the default welcome note

--- src/store/modules/notes/index.js
+++ src/store/modules/notes/index.js
@@ -17,7 +17,7 @@ export default {
           "Here are some 'notable' tips for you.\n\n" +
           "Pressing Enter while creating or editing a note will create a new line hence you can have notes with multiple paragraphs.\n\n" +
           'To add a new note, simply click the "New Note" button on the Sidebar. You can also delete an existing note by clicking the "Delete" button at the top right of an open notes window, and all you have to do to edit an existing note is to click the "Edit" button below to bring up the edit field.\n\n' +
-          "Please note: If you choose to delete or edit this Welcome note, you can restore it at any time from the Settings page.\n\n\n\n\n" +
+          "Please note: If you choose to delete this Welcome Note, you can restore it at any time from the Settings page.\n\n\n\n" +
           "Marking my spot in the world of web development,\n" +
           "Allan Kirui.",
         isDefault: true,
