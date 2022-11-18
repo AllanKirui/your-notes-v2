4ddf4ba0d24c93ef48f5da581ad1fc6323d3ea16
AllanKirui <nallawilljr@gmail.com>
2022-11-18 13:48:59 +0300

    Add update to logic for creating and signing in into accounts

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -6,106 +6,106 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 
+import { throwException } from "@/store/index.js";
+
 export default {
-  createAccount(_, payload) {
-    createUserWithEmailAndPassword(getAuth(), payload.email, payload.password)
-      .then((data) => {
-        // TODO: redirect the user to the todo page
-        // TODO: show loading spinner
+  async createAccount(_, payload) {
+    try {
+      await createUserWithEmailAndPassword(
+        getAuth(),
+        payload.email,
+        payload.password
+      );
 
-        console.log(data);
-        alert("Successfully registered");
-        // console.log(getAuth().currentUser);
-      })
-      .catch((error) => {
-        // context.commit("setError", error);
-        alert(error.message);
-        console.log(error);
-      });
+      // TODO: redirect the user to the todo page
+      // TODO: show loading spinner
+    } catch (error) {
+      throwException(error, "createUserWithEmailAndPassword( ) fn");
+    }
   },
-  createAccountWithGoogle() {
+  async createAccountWithGoogle() {
     // create a provider when the sign in button is clicked
     const provider = new GoogleAuthProvider();
-    signInWithPopup(getAuth(), provider)
-      .then((result) => {
-        // TODO: redirect the user to the todo page
-        // TODO: show loading spinner
-        console.log(result);
-      })
-      .catch((error) => {
-        alert("Oops! Problem signing in with Google. Try again.");
-        console.log(error);
-      });
+
+    try {
+      await signInWithPopup(getAuth(), provider);
+
+      // TODO: redirect the user to the todo page
+      // TODO: show loading spinner
+    } catch (error) {
+      // alert("Oops! Problem signing in with Google. Refresh the page and try again.");
+      throwException(error, "createAccountWithGoogle( ) fn");
+    }
   },
-  signIn(context, payload) {
-    signInWithEmailAndPassword(getAuth(), payload.email, payload.password)
-      .then(() => {
-        // TODO: store user data locally as shown in with Google
-        // TODO: what if multiple user's sign in on the same computer,
-        // will one user's sign in overwite another user's preferences?
-        // TODO: redirect the user to the todo page
-        // TODO: show loading spinner
-        // dispatch an action to store an authenticated user's data
-        // context.dispatch("createUserProfile", getAuth().currentUser);
-      })
-      .catch((error) => {
-        switch (error.code) {
-          case "auth/invalid-email":
-            context.commit("setError", {
-              message: "Email is not properly formatted",
-              field: "email",
-            });
-            break;
-          case "auth/user-not-found":
-            context.commit("setError", {
-              message: "No account with that email was found",
-              field: "email",
-            });
-            break;
-          case "auth/wrong-password":
-            // TODO: Show 'forgot password' link
-            context.commit("setError", {
-              message: "Incorrect password",
-              field: "password",
-            });
-            break;
-          default:
-            // set the error on both fields
-            context.commit("setError", {
-              message: error.code,
-              field: "email",
-            });
-            context.commit("setError", {
-              message: error.code,
-              field: "password",
-            });
-            break;
-        }
-      });
+  async signIn(context, payload) {
+    try {
+      await signInWithEmailAndPassword(
+        getAuth(),
+        payload.email,
+        payload.password
+      );
+
+      // TODO: store user data locally as shown in with Google
+      // TODO: what if multiple user's sign in on the same computer,
+      // will one user's sign in overwite another user's preferences?
+      // TODO: redirect the user to the todo page
+      // TODO: show loading spinner
+    } catch (error) {
+      switch (error.code) {
+        case "auth/invalid-email":
+          context.commit("setError", {
+            message: "Email is not properly formatted",
+            field: "email",
+          });
+          break;
+        case "auth/user-not-found":
+          context.commit("setError", {
+            message: "No account with that email was found",
+            field: "email",
+          });
+          break;
+        case "auth/wrong-password":
+          // TODO: Show 'forgot password' link
+          context.commit("setError", {
+            message: "Incorrect password",
+            field: "password",
+          });
+          break;
+        default:
+          // set the error on both fields
+          context.commit("setError", {
+            message: error.code,
+            field: "email",
+          });
+          context.commit("setError", {
+            message: error.code,
+            field: "password",
+          });
+          break;
+      }
+    }
   },
-  signInWithGoogle(context) {
+  async signInWithGoogle(context) {
     // create a provider when the sign in button is clicked
     const provider = new GoogleAuthProvider();
-    signInWithPopup(getAuth(), provider)
-      .then(() => {
-        let storedData = JSON.parse(
-          localStorage.getItem("yourNotesPreferences")
-        );
+    try {
+      await signInWithPopup(getAuth(), provider);
 
-        // dispatch an action to update the user data stored locally
-        context.dispatch(
-          "updateLocalStorageData",
-          {
-            isLoggedIn: true,
-            theme: storedData.theme || null,
-            fontSize: storedData.fontSize || 14,
-          },
-          { root: true }
-        );
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      let storedData = JSON.parse(localStorage.getItem("yourNotesPreferences"));
+
+      // dispatch an action to update the user data stored locally
+      context.dispatch(
+        "updateLocalStorageData",
+        {
+          isLoggedIn: true,
+          theme: storedData.theme || null,
+          fontSize: storedData.fontSize || 14,
+        },
+        { root: true }
+      );
+    } catch (error) {
+      throwException(error, "signInWithGoogle( ) fn");
+    }
   },
   createUserProfile(context, payload) {
     // sets the current user to the active account
