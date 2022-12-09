46c735d374b03825026aa62975ce7593ce611e23
AllanKirui <nallawilljr@gmail.com>
2022-12-09 14:15:56 +0300

    Add update to config file

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
-module.exports = {
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/your-notes-v2/" : "/",
-};
+});
